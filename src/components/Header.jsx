f84022f1aadf3dcdd0d09586e8edcbfe90acdc60
alkhlal00 <alkhlaljn@gmail.com>
2025-11-21 18:21:30 +0100

    add dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,6 +44,24 @@ const Header = () => {
               </Link>
             );
           })}
+      <div className="relative group text-[#333] p-2">
+
+  <span className="cursor-pointer">Services</span>
+
+  <ul
+    className="
+      absolute left-0 mt-2 w-40 bg-white text-black rounded shadow-lg 
+      opacity-0 invisible group-hover:opacity-100 group-hover:visible 
+      transition-all duration-200 z-20
+    "
+  >
+    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Web Development</li>
+    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Design</li>
+    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">SEO</li>
+  </ul>
+
+</div>
+
         </nav>
 
         <div className='flex items-center justify-center '>
